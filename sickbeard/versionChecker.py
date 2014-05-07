96365ed831ed2cef0eaaaed9bd70280ff4a29a13
mr-orange <mr_orange@hotmail.it>
2014-05-07 12:27:40 +0200

    Version Changing fro mr-orange fork.

--- sickbeard/versionChecker.py
+++ sickbeard/versionChecker.py
@@ -122,10 +122,10 @@ class CheckVersion():
 class UpdateManager():
 
     def get_github_repo_user(self):
-        return 'mr-orange'
+        return 'Octpussy76'
 
     def get_github_repo(self):
-        return 'Sick-Beard'
+        return 'Sickbeard'
 
     def get_update_url(self):
         return sickbeard.WEB_ROOT + "/home/update/?pid=" + str(sickbeard.PID)
